3507418b181ddb6ff55fed2e1b43e8a31e766d1d
Mircea Casapu <mircea.casapu@gmail.com>
2021-08-31 10:14:10 +0300

    added base commands and a few npm packages

---
+++ deploy-commands.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { REST } = require("@discordjs/rest");
+const { Routes } = require("discord-api-types/v9");
+const token = process.env.TOKEN;
+const clientId = process.env.CLIENT_ID;
+const guildId = process.env.GUILD_ID;
+
+const commands = [
+  new SlashCommandBuilder()
+    .setName("ping")
+    .setDescription("Replies with pong!"),
+  new SlashCommandBuilder()
+    .setName("server")
+    .setDescription("Replies with server info!"),
+  new SlashCommandBuilder()
+    .setName("user")
+    .setDescription("Replies with user info!"),
+].map((command) => command.toJSON());
+
+const rest = new REST({ version: "9" }).setToken(token);
+
+(async () => {
+  try {
+    await rest.put(Routes.applicationGuildCommands(clientId, guildId), {
+      body: commands,
+    });
+
+    console.log("Successfully registered application commands.");
+  } catch (error) {
+    console.error(error);
+  }
+})();
